a4eaa66a9892c357f9e114515cd81b6e78eb8092
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-09-25 19:35:51 +0100

    Add comments.py file

---
+++ code-start/01_hello/comments.py
@@ -0,0 +1,31 @@
+# =======================================================================
+# Author: Dr. Saad Laouadi
+# Course: Python-101
+# Lesson: Comments in Python
+#
+# Description:
+# -----------
+#   This script provides an overview of how to use different types of comments in
+#   Python, including single-line comments, multi-line comments, and docstrings.
+#
+# © Copyright: Dr. Saad Laouadi
+# =======================================================================
+
+# 1. Single-line Comments:
+
+
+# Example of a single-line comment:
+
+# Single-line comments can also be used to explain variables:
+
+# 2. Multi-line Comments:
+# -----------------------
+
+# Another common approach is to use multiple single-line comments:
+
+# 3. Documentation Strings (Docstrings):
+
+
+# Example function with a docstring:
+
+# 4. Inline Comments:
