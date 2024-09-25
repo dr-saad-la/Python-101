530f465a51a881eea28c3740d0471f7df3c2f24d
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-09-25 19:48:55 +0100

    Add hellp.py file

---
+++ code-finished/01_hello/hello.py
@@ -0,0 +1,34 @@
+# =======================================================================
+# Author: Dr. Saad Laouadi
+# Course: Python-101
+# Lesson: Hello World in Python
+#
+# Description:
+# This script demonstrates the classic "Hello, World!" program in Python.
+# It is a simple example to introduce beginners to the syntax of Python.
+# =======================================================================
+
+
+def main():
+    """
+    Main function that prints 'Hello, World!' to the console.
+
+    This is a basic example of a Python program. It introduces the concept
+    of a function, printing output to the console, and the structure of
+    a Python script.
+
+    No arguments or return values.
+    """
+    # Print a greeting message to the console
+    print("Hello, World!")
+
+
+# This is the standard boilerplate to call the main() function.
+# It ensures that the script runs when executed directly.
+if __name__ == "__main__":
+    main()
+
+# =======================================================================
+# © Copyright: Dr. Saad Laouadi
+# All Rights Reserved 🛡️
+# =======================================================================
---
+++ code-start/01_hello/hello.py
@@ -0,0 +1,16 @@
+# =======================================================================
+# Author: Dr. Saad Laouadi
+# Course: Python-101
+# Lesson: Hello World in Python
+#
+# Description:
+# This script demonstrates the classic "Hello, World!" program in Python.
+# It is a simple example to introduce beginners to the syntax of Python.
+# =======================================================================
+
+
+
+# =======================================================================
+# © Copyright: Dr. Saad Laouadi
+# All Rights Reserved 🛡️
+# =======================================================================
